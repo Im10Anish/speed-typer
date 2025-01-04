9e98c00cf29a78b122908a030111700b5a877063
Im10Anish <im.10.anish@gmail.com>
2025-01-04 17:01:28 +0530

    add texts

---
+++ src/app/data/index.ts
@@ -0,0 +1,9 @@
+import shortTexts from "./short.json";
+import mediumTexts from "./medium.json";
+import longTexts from "./long.json";
+
+export const TYPING_TEXTS = {
+  short: shortTexts,
+  medium: mediumTexts,
+  long: longTexts,
+};
